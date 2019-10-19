69e80648a8cc4204ba94443458d1f6a4e02d5b04
nsaip <nadiasaip@gmail.com>
2019-10-19 17:55:21 -0500

    variables.tf changes made in file

---
+++ deployment/terraform/variables.tf
@@ -0,0 +1,31 @@
+variable "name" {
+  default = "api-webplatform"
+}
+variable "chart" {
+    default = "./api-webplatform"
+  
+}
+variable "version" {
+    default = "6.0.1"
+  
+}
+variable "deployment_image" {
+  default = "nextcloud"
+  
+}
+  
+
+variable "deployment_environment" {
+  default = "dev"
+}
+
+
+variable "deployment_endpoint" {
+  type = "map"
+
+  default = {
+    dev  = "dev.api-platforms.fuchicorp.com"
+    qa   = "qa.api-platforms.fuchicorp.com"
+    prod = "api-platforms.fuchicorp.com"
+  }
+}
