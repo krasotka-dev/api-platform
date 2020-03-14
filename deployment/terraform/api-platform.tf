9f9075612d7217f1ee3f526f84cc6cdfbc3d97e9
ana <anastasiaclipco@gmail.com>
2020-03-13 23:16:47 -0500

    edited api_platform.yaml

--- deployment/terraform/api-platform.tf
+++ deployment/terraform/api-platform.tf
@@ -1,24 +1,12 @@
+module "api_deploy" {
+  source  = "fuchicorp/chart/helm"
 
-data "template_file" "api-platform_values" {
-  template = "${file("./api-platform/template_values.yaml")}"
-  vars = {
-    deployment_image = "${var.deployment_image}"
-    deployment_endpoint = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
+  deployment_name        = "api_platform"
+  deployment_environment = "${var.deployment_environment}"
+  deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
+  deployment_path        = "api-platform"
 
+  template_custom_vars  = {
+    deployment_image    = "${var.deployment_image}"
   }
-}
-
-resource "local_file" "api-platform_values_local_file" {
-  content  = "${trimspace(data.template_file.api-platform_values.rendered)}"
-  filename = "./api-platform/.cache/values.yaml"
-}
-resource "helm_release" "api-platform" {
-  name       = "${var.name}"
-  chart      = "${var.chart}"
-  version    = "${var.version}"
-  namespace = "${var.deployment_environment}"
-
- values = [
-    "${data.template_file.api-platform_values.rendered}"
-  ]
 }
