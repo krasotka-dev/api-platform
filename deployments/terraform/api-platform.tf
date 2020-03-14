220ae11f0138ad2223cc88c0ab5b3b8c8b1619e8
ana <anastasiaclipco@gmail.com>
2020-03-13 23:29:48 -0500

    naming changed

--- deployments/terraform/api-platform.tf
+++ deployments/terraform/api-platform.tf
@@ -1,7 +1,7 @@
-module "api_deploy" {
+module "api-deploy" {
   source  = "fuchicorp/chart/helm"
 
-  deployment_name        = "api_platform"
+  deployment_name        = "api-platform"
   deployment_environment = "${var.deployment_environment}"
   deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
   deployment_path        = "api-platform"
